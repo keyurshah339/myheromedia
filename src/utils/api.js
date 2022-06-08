4fb56aff3cff098b922ed0a00435c088ec48802f
Keyur Shah <75299295+keyurshah339@users.noreply.github.com>
2022-06-09 00:10:46 +0530

    bugfix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const setInitialAPI = () => {
-  axios.defaults.baseURL = "https://universely.herokuapp.com/";
+  axios.defaults.baseURL = "https://agile-headland-48240.herokuapp.com/";
   axios.defaults.headers.common["Authorization"] = JSON.parse(
     localStorage.getItem("authToken")
   );

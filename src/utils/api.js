0bb23b8dd181bbf743a4741df85e3f064b99dcf8
Keyur Shah <75299295+keyurshah339@users.noreply.github.com>
2023-02-28 01:12:34 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const setInitialAPI = () => {
-  axios.defaults.baseURL = "https://agile-headland-48240.herokuapp.com/";
+  axios.defaults.baseURL = "https://myheromedia-backend.vercel.app/";
   axios.defaults.headers.common["Authorization"] = JSON.parse(
     localStorage.getItem("authToken")
   );
